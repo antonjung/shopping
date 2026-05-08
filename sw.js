40b04906da092e08e9cbdd66826bbb1b24242ed8
JUNGLEMAJOR1\anton <anton@junglemajor.co.uk>
2026-05-08 16:05:04 +0100

    Bump service worker cache to v3 to force PWA update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shopping-v2';
+const CACHE = 'shopping-v3';
 const ASSETS = ['./index.html', './app.css', './app.js', './manifest.json', './icon.svg', './firebase-config.js'];
 
 self.addEventListener('install', e => {
